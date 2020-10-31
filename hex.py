00e4dbe17c0ccc7a28936df9645630264e3c93d1
Zoe <zpiccirillo10@stuy.edu>
2020-10-30 21:21:19 -0400

    add stub for reading input files

--- hex.py
+++ hex.py
@@ -1,18 +1,4 @@
-'''
 import sys
-mode = sys.argv[1]
-keyfile = sys.argv[2]
-inpfile = sys.argv[3]
-key = open(keyfile).read()[:-1] #removes the mandatory \n at the end of the file to support one line messages.
-inp = open(inpfile).read()[:-1] #removes the mandatory \n at the end of the file to support one line messages.
-debug = False
-
-if(debug):
-    print("mode:"+mode)
-    print("key: "+key)
-    print("inp: "+inp)
-'''
-
 
 def solve(key_word, inp):
     key = key_word
@@ -26,8 +12,20 @@ def solve(key_word, inp):
     str = ""
     for i in range(len(inp)):
         str += chr(ord(inp[i]) ^ ord(key[i]))
-    print(inp)
     print(str)
 
-solve("A", "hello")
-solve("FISH", " this is a test")
+if __name__ == "__main__":
+    mode = sys.argv[1]
+    keyfile = sys.argv[2]
+    inpfile = sys.argv[3]
+    key = open(keyfile).read()[:-1] #removes the mandatory \n at the end of the file to support one line messages.
+    inp = open(inpfile).read()[:-1] #removes the mandatory \n at the end of the file to support one line messages.
+    debug = False
+
+    if(debug):
+        print("mode:"+mode)
+        print("key: "+key)
+        print("inp: "+inp)
+
+    #note to self: incorporate mode into argument once finished w part 2
+    solve(key, inp)
