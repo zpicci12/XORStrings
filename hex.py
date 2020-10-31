e77ab84ea8f444da47a50bc2371e0cf4d5f8d495
Zoe <zpiccirillo10@stuy.edu>
2020-10-31 16:45:51 -0400

    update documentation

--- hex.py
+++ hex.py
@@ -1,37 +1,31 @@
+'''
+Zoe Piccirillo
+Cybersecurity Period 5
+November 2, 2020
+
+XORStrings: encode/decode messages using the XOR operator.
+
+Usage: "make run ARGS="mode keyfile textfile"
+'''
+
 import sys
 
-def solve(mode, key_word, inp):
+def solve(mode, key_word, inp): #perform XOR operation on key and input text
     key = key_word
     count = 0
-    while (len(key) < len(inp)):
+    while (len(key) < len(inp)): #loop key until it is the length of the input text
         key = key + key_word[count]
         if (count == len(key_word) - 1):
             count = 0
         else:
             count += 1
     str = ""
-    for i in range(len(inp)):
+    for i in range(len(inp)): #XOR each character of input text and key; convert to ASCII (human mode) or hex value (numOut)
         if (mode == "human"):
             str += chr(ord(inp[i]) ^ ord(key[i]))
         else:
-            str += (hex(ord(inp[i]) ^ ord(key[i])))[2:] + " "
+            str += (hex(ord(inp[i]) ^ ord(key[i])))[2:] + " " #remove ("0x") from beginning of hex value
     print(str)
-
-'''
-def solve_hex(key_word, inp):
-    key = key_word
-    count = 0
-    while (len(key) < len(inp)):
-        key = key + key_word[count]
-        if (count == len(key_word) - 1):
-            count = 0
-        else:
-            count += 1
-    str = ""
-    for i in range(len(inp)):
-        str += (hex(ord(inp[i]) ^ ord(key[i])))[2:] + " "
-    print(str)
-'''
 
 if __name__ == "__main__":
     mode = sys.argv[1]
@@ -46,10 +40,7 @@ if __name__ == "__main__":
         print("key: "+key)
         print("inp: "+inp)
 
-    #note to self: incorporate mode into argument once finished w part 2
     if (mode == "numOut" or mode == "human"):
         solve(mode, key, inp)
-    else:
+    else: #mode is not "numOut" or "human"
         print("Not a valid mode. Please select 'human' or 'numOut.'")
-
-    #solve_hex(mode, key, inp)
