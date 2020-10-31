cb5df2d3dd06ce3a274707ab4777d8386963939a
Zoe <zpiccirillo10@stuy.edu>
2020-10-31 16:14:39 -0400

    add method solve_hex for numOut

--- hex.py
+++ hex.py
@@ -14,6 +14,20 @@ def solve(key_word, inp):
         str += chr(ord(inp[i]) ^ ord(key[i]))
     print(str)
 
+def solve_hex(key_word, inp):
+    key = key_word
+    count = 0
+    while (len(key) < len(inp)):
+        key = key + key_word[count]
+        if (count == len(key_word) - 1):
+            count = 0
+        else:
+            count += 1
+    str = ""
+    for i in range(len(inp)):
+        str += (hex(ord(inp[i]) ^ ord(key[i])))[2:] + " "
+    print(str)
+
 if __name__ == "__main__":
     mode = sys.argv[1]
     keyfile = sys.argv[2]
@@ -29,3 +43,4 @@ if __name__ == "__main__":
 
     #note to self: incorporate mode into argument once finished w part 2
     solve(key, inp)
+    solve_hex(key, inp)
